5a7f435e5747003761976e37a8a048081b5b8f85
Daniel Grunkin <danielgrunkin@gmail.com>
2020-02-23 10:19:05 +0100

    add stones

---
+++ lvls/stoned.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="stoned" tilewidth="80" tileheight="80" tilecount="9" columns="3">
+ <image source="../assets/art/stoned.png" width="240" height="240"/>
+ <terraintypes>
+  <terrain name="Stone" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain=",0,,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,,0,"/>
+ <tile id="6" terrain=",0,,"/>
+ <tile id="7" terrain="0,0,,"/>
+ <tile id="8" terrain="0,,,"/>
+</tileset>
