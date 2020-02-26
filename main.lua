538b9ce9895812f010068e6635b9edbf4b54b7f0
JakobSchauser <jakob.schauser@gmail.com>
2020-02-26 08:33:37 +0100

    Made export-ready

--- main.lua
+++ main.lua
@@ -32,7 +32,7 @@ function love.load()
   --windowWidth, windowHeight = windowWidth * .7, windowHeight * .7 --make the window a bit smaller than the screen itself
   push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true})
   screen:setDimensions(push:getDimensions())
-  Gamestate.switch(game)
+  Gamestate.switch(splash)
 end
 
 function love.draw()
@@ -64,7 +64,7 @@ function love.keypressed(key)
     love.event.quit()
   end
 
-  if key == "d" then
+  if key == "d" and false then
     debug = not debug
   end
 
